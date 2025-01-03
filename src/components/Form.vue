0cfc50c654c6327370c2966b1f3d1ceb56a4c37c
vinimercesbr <vinimerces2016@gmail.com>
2025-01-03 20:13:21 -0300

    build

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="banner" class="items-center text-center touch-none p-6 max-mobile:p-3 m-0 desktop:h-full desktop:w-full  bg-[url('/pattern-bg-desktop.png')] max-mobile:bg-[url('/pattern-bg-mobile.png')]">
-    <h1 class="text-white p-3 max-[375px]:p-5 text-center font-semibold text-[25px] touch-none">IP Address Tracker</h1>
-    <form class="search-form text-center flex  min-[1440px]:w-[70%] min-[1440px]:h-[25%] pt-0 ml-2 min-[1440px]:m-2 min-[1440px]:inline-flex mt-2 p-5 mb-5" @submit.prevent="submitForm">
-      <input type="text" class="search-form__box p-2 rounded-l-xl focus:outline-none min-w-[65%] inline-flex h-[55px] min-[1440px]:w-full -ml-2 mb-5 min-[1440px]:m-4 touch-none;" v-model="ipInput" placeholder="Search for any IP address or domain">
+    <h1 class="text-white p-3 max-mobile:p-5 text-center font-semibold text-[25px] touch-none">IP Address Tracker</h1>
+    <form class="search-form text-center flex  desktop:w-[70%] desktop:h-[25%] pt-0 ml-2 desktop:m-2 desktop:inline-flex mt-2 p-5 mb-5" @submit.prevent="submitForm">
+      <input type="text" class="search-form__box p-2 rounded-l-xl focus:outline-none min-w-[65%] inline-flex h-[55px] desktop:w-full -ml-2 mb-5 desktop:m-4 touch-none;" v-model="ipInput" placeholder="Search for any IP address or domain">
       <button class="search-form__button rounded-r-xl stroke-2 mr-1 h-[55px] p-4 bg-black" type="submit">
         <img src="/icon-arrow.svg" alt="enviar">
       </button>
